57b578555ca77d19c9003627d0658f2449214514
sfincks <sfincks666@gmail.com>
2024-04-02 08:17:05 -0700

    init

---
+++ Hw1/Hw1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hw1
+//
+//  Created by Артур on 02.04.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
